a03f692e46b40a09bf17488a5914a9cf62f3c01f
AnggiSatria <anggisatria122@gmail.com>
2025-10-03 23:47:15 +0700

    feat(master): add fifth init

--- src/packages/service/get-users/fetcher.ts
+++ src/packages/service/get-users/fetcher.ts
@@ -3,7 +3,9 @@ import type { GitHubUser } from "../interface/user.interface";
 export async function fetchUsers(query: string): Promise<GitHubUser[]> {
   if (!query) return [];
   const res = await fetch(
-    `https://api.github.com/search/users?q=${query}&per_page=5`
+    `${
+      import.meta.env.VITE_GITHUB_API || "https://api.github.com/search/users"
+    }?q=${query}&per_page=5`
   );
   if (!res.ok) throw new Error("Failed to fetch users");
   const data = await res.json();
